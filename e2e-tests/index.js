b53643ecaa4caa9b15270281f9615b8858654e79
Abi Hafshin <abi.alfarouq@traveloka.com>
2017-10-05 01:36:44 +0700

    fail e2e

--- e2e-tests/index.js
+++ e2e-tests/index.js
@@ -3,5 +3,5 @@ import { Selector } from "testcafe";
 fixture("Homepage").page("http://localhost:3000/");
 
 test("Stupid test", async t => {
-  await t.expect(Selector(".uit-hello").innerText).eql("Hello World!");
+  await t.expect(Selector(".uit-hello").innerText).eql("Hello World?");
 });
